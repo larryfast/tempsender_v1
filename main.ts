94327ef1cd2257c83396afe681acf1244a82ad73
larryfast <larry@legible.com>
2023-04-04 01:05:11 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let temperature = 0
+radio.setGroup(1)
+basic.forever(function () {
+    temperature = input.temperature()
+    radio.sendValue(control.deviceName(), temperature)
+    basic.showString("" + (temperature))
+    basic.pause(2000)
+})
