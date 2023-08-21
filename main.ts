854a21701023ef3c5ae1df664c17350db2c00a7a
larryfast <larry@legible.com>
2023-08-20 19:32:12 -0700

    Added screen displays for Radio 0

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 let temperature = 0
-radio.setGroup(1)
+radio.setGroup(0)
 basic.forever(function () {
+    basic.showLeds(`
+        . # # . .
+        # . . # .
+        # . . # .
+        # . . # .
+        . # # . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
     temperature = input.temperature()
     radio.sendValue(control.deviceName(), temperature)
     basic.showString("" + (temperature))
-    basic.pause(500)
 })
